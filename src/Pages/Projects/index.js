247528dfaddc5d7dd814fc299afaf2f3828dfab0
Asher Beck <117310101+Asherbeck10@users.noreply.github.com>
2023-03-05 15:45:39 +0000

    added data to json

--- src/Pages/Projects/index.js
+++ src/Pages/Projects/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ProjectGallery from '../../Components/ProjectGallery'
-import Scheduler from '../../assets/schedular.png'
 import './style.css'
-//çimport projectList from '../../projectsList.json'
+import projectsList from '../../projectsList.json'
+
 function Projects() {
   return (
     <div className='projects'>
       <h1>My Projects</h1>
       <div className='projectList'>
-        <ProjectGallery name="Schedular" image={Scheduler}/>
+        {projectsList.map((project)=>{
+          return <ProjectGallery name={project.name} image={project.image}/>
+        })}
       </div>
 
     </div>
