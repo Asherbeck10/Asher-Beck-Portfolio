e63b19592e33b4ab37a5c35e07a6bf90953a684b
Asher Beck <117310101+Asherbeck10@users.noreply.github.com>
2023-03-05 07:22:01 +0000

    added responsive ellements

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React,{useState,useEffect} from 'react'
+import {Link,useLocation} from 'react-router-dom'
 import './style.css'
 import { FaBars} from 'react-icons/fa'
 
 
+
 function NavBar() {
+    const[expandNavbar,setExpandNavbar]=useState(false)
+    // info on which route you in and accordingly  close the navbar
+    const location=useLocation()
+    useEffect(()=>{
+        setExpandNavbar(false)
+     
+    },[location])
   return (
-    <div className="navbar">
+    //open or close hamburger button
+    <div className="navbar" id={expandNavbar ? "open" :"close"}>  
         <div className='toggleButton'>
-            <button><FaBars/> </button>
+            <button
+            //change the state on click 
+            onClick={()=>{
+                setExpandNavbar((prev)=> !prev);
+            }}
+            >    <FaBars/> </button>
         </div>
-        <div className='"Links'>
+        <div className='links'>
             <Link to="/">Home</Link>
             <Link to="contact">Contact</Link>
             <Link to="/project">Project</Link>
