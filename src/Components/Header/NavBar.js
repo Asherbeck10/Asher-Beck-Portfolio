39b029d160819a84f7e16809f094aed69d532100
Asher Beck <117310101+Asherbeck10@users.noreply.github.com>
2023-03-05 00:24:36 +0000

    added hamburger button

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './style.css'
-
+import { FaBars} from 'react-icons/fa'
 
 
 function NavBar() {
   return (
     <div className="navbar">
         <div className='toggleButton'>
-            <button>ReorderIcon </button>
+            <button><FaBars/> </button>
         </div>
         <div className='"Links'>
-            <Link to="contact">Contact</Link>
             <Link to="/">Home</Link>
+            <Link to="contact">Contact</Link>
             <Link to="/project">Project</Link>
+            
         </div>
     </div>
      )
