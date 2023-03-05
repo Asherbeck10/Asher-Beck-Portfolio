e5d5289f9031cdc5b3886e2283de9f1cbc67ca0d
Asher Beck <117310101+Asherbeck10@users.noreply.github.com>
2023-03-05 09:27:40 +0000

    added links

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React from 'react';
+import './style.css';
+import {FaGithub,FaLinkedin} from 'react-icons/fa'
+import{AiTwotoneMail} from 'react-icons/ai'
+import {FcDocument} from 'react-icons/fc'
 function Home() {
   return (
-    <div>Home</div>
+    <div className='"home'>
+      <div className="about">
+      <h2>Hello, My Name is Asher</h2>
+      <div className='prompt'>
+        <p>here will be something about me </p>
+        <a href="https://github.com/Asherbeck10" target="_blank"  rel="noreferrer"><FaGithub/></a>
+        <AiTwotoneMail/>
+       <a href="https://www.linkedin.com/in/asher-beck-276454259/" target="_blank"  rel="noreferrer" > <FaLinkedin/></a>
+        <FcDocument/>
+          
+      </div>
+
+      </div>
+    </div>
       
    
   )
 }
 
 export default Home
+
