277d974f6a80929c3eb15489ee5f74f5c70a7daf
Asher Beck <117310101+Asherbeck10@users.noreply.github.com>
2023-03-06 14:52:26 +0000

    added form with validation

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import "./style.css";
 
-const Contact = () => {
+
+const schema = yup.object().shape({
+  firstName: yup.string().required("First Name is Required"),
+  lastName: yup.string().required(),
+  email: yup.string().email().required(),
+  content: yup.string().min(25).max(150).required(),
+  
+  
+}).required();
+
+const Contact=()=> {
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  
+
   return (
-    <div>Contact</div>
-      
-   
-  )
+    <div className="Form">
+      <div className="title">Contact Me</div>
+      <div className="inputs">
+        <form onSubmit={handleSubmit(data=>console.log(data))}>
+          <input
+            type="text"
+            {...register("firstName")}
+            // name="firstName"
+            // ref={register}
+            placeholder="First Name..."
+          />
+         
+          <input
+            type="text"
+            // name="lastName"
+            {...register("lastName")}
+            placeholder="Last Name..."
+            // ref={register}
+          />
+         
+          <input
+            type="text"
+            {...register("email")}
+            // name="email"
+            placeholder="Email..."
+            // ref={register}
+          />
+          
+          <input
+            type="text"
+            // name='password'
+            {...register("content")}
+            placeholder="..."
+            // ref={register}
+          />
+          
+          
+          <input type="submit" id="submit" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
