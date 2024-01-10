b521865424e3defd9a1deceb43a9b8bdfbfd726f
Asherbeck10 <asher.bec@gmail.com>
2024-01-10 11:31:20 +0000

    update google analytic

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Contact from './Pages/Contact/';
 import Home from './Pages/Home/';
 import Project from './Pages/Projects';
-import NavBar from './Components/Header/NavBar'
+import NavBar from './Components/Header/NavBar';
 import ProjectInfo from './Pages/ProjectInfo';
-
+import TagManager from 'react-gtm-module';
+const tagManagerArgs = {
+  gtmId: 'GTM-THQP9Z47',
+};
+TagManager.initialize(tagManagerArgs);
 function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar/>
+        <NavBar />
         <Routes>
-        <Route path="/Contact" element={<Contact/>}/>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/project" element={<Project/>}/>
-          <Route path="/project/:id" element={<ProjectInfo/>}/>
-          
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/project/:id" element={<ProjectInfo />} />
         </Routes>
       </Router>
     </div>
@@ -24,5 +27,3 @@ function App() {
 }
 
 export default App;
-
-
